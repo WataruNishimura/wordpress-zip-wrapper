04b0dfcf89955abce3d6dd87d4ad8a4305b8fb13
WataruNishimura <wataru.nishimura@nnkk.work>
2021-02-03 14:47:37 +0900

    arg parser for argument management

--- init.py
+++ init.py
@@ -1,4 +1,19 @@
 import pathlib
+import argparse
+
+# argparse init parser 
+parser = argparse.ArgumentParser(description="sample description")
+parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+args = parser.parse_args()
+
+print(args)
+
+exclude_file_path = pathlib.Path(".zipexclude")
+exclude_file_flag = False
+
+if(exclude_file_path.exists()):
+  print("Exclude file exists")
+  exclude_file_flag = True
 
 path = pathlib.Path("test")
 pathlib_files = path.glob("**/*")
