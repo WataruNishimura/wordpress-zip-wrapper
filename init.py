053b187309d5993c79429ab62ee72daa10ab8d04
ONC-HNIW <onc.gi@nnkk.work>
2021-03-09 13:54:00 +0900

    update init.py

--- init.py
+++ init.py
@@ -3,12 +3,13 @@ import argparse
 
 # CLI: for parse args
 parser = argparse.ArgumentParser(description="sample description")
-parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+#parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+parser.add_argument('input', help='Set target folder')
+parser.add_argument('output', help='Set output zip file name')
+parser.add_argument('-e','--exclude', help='Exclude target from the given file.', default='.zipexclude')
 args = parser.parse_args()
 
-print(args)
-
-exclude_file_path = pathlib.Path(".zipexclude")
+exclude_file_path = pathlib.Path(args.exclude)
 exclude_file_flag = False
 
 if(exclude_file_path.exists()):
@@ -16,7 +17,7 @@ if(exclude_file_path.exists()):
   exclude_file_flag = True
 
 # Get file and folder list in specific directories.
-path = pathlib.Path("test")
+path = pathlib.Path(args.input)
 pathlib_files = path.glob("**/*")
 
 for file in list(pathlib_files):
